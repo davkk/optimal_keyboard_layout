a0999d47fae50df957cd89670e82a62894d10024
davkk <dadulerk@gmail.com>
2023-12-26 20:38:13 +0100

    compress dataset, add chars histogram script

---
+++ bin/hist.ml
@@ -0,0 +1,19 @@
+module CharMap = Map.Make (Char)
+
+let () =
+  Stdio.In_channel.with_file "macbeth.txt" ~f:In_channel.input_all
+  |> String.fold_left
+       (fun map c ->
+         CharMap.update
+           c
+           (function
+             | Some value -> Some (value + 1)
+             | None -> Some 1)
+           map)
+       CharMap.empty
+  |> CharMap.to_list
+  |> List.sort (fun (_, count1) (_, count2) -> -compare count1 count2)
+  |> List.to_seq
+  |> Seq.take 10
+  |> Seq.iter (fun (c, count) -> Fmt.pr "%c: %d@." c count)
+;;
